7c12c65b565d1bbffd19d53f12c278f54c7f518a
Efe Polat <mefepolat@hotmail.com>
2022-09-18 20:28:31 -0400

    fetched more data for champion details

--- controllers/champions.js
+++ controllers/champions.js
@@ -21,13 +21,19 @@ export async function championDetails(req,res){
     }
     let champion;
     console.log(Object.keys(championList.data[trimmed]))
+    console.log(Object.values(championList.data[trimmed]))
     if(championList.data[trimmed] === undefined){
       return res.redirect('/champions');
     }else {
       champion = championList.data[trimmed];
+      const response2 = await fetch(`https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champions/${champion.key}.json`);
+      let championData = await response2.json();
+      console.log(Object.keys(championData))
+      console.log(Object.values(championData['spells'][0]))
+      championData = Object.values(championData)
       const fileName = champion.key.toString().padEnd(champion.key.length + 3, '000')
       const imgSrc = `https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-tiles/${champion.key}/${fileName}.jpg`
-      res.render('details.ejs', {champion, imgSrc})
+      res.render('details.ejs', {champion, imgSrc, championData})
     }
     
     
